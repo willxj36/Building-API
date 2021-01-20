ae550eda44567fb17b3b751a5652c9207758590e
willxj36 <willxj36@gmail.com>
2021-01-20 16:27:16 -0700

    finished all of lab

--- public/app.js
+++ public/app.js
@@ -1,24 +1,60 @@
 const getChirps = () => {
+    $('.chirp').off();
+    $('.x').off();
     $.get('http://localhost:3000/api/chirps')
     .then(chirpRes => {
-        let chirpArray = Object.values(chirpRes);
-        console.log(chirpArray);
-        let chirpHtml = chirpArray.map((chirp, index) => {
-            if(chirp.name) { //eliminates the 'undefined' chirp produced by the nextid object
+        let chirpArray = Object.entries(chirpRes);
+        let chirpHtml = chirpArray.map((chirp) => {
+            if(chirp[1].name) { //eliminates the 'undefined' chirp produced by the nextid object
                 return (
-                    `<div class="row" id="row${chirpArray[chirpArray.length - 1] - (chirpArray.length - 1) + index}">
+                    `<div class="row chirp" id="row${chirp[0]}">
                         <div class="card col-10 border-danger shadow m-3 p-1">
                             <div class="card-body">
-                                <h2 class="card-title">${chirp.name}<h3>
-                                <p class="card-text text-sm">${chirp.text}</p>
+                                <h2 class="card-title" id="row${chirp[0]}name">${chirp[1].name}<h3>
+                                <p class="card-text text-sm" id="row${chirp[0]}chirp">${chirp[1].text}</p>
                             </div>
                         </div>
-                        <div class="col-1 x" id="${chirpArray[chirpArray.length - 1] - (chirpArray.length - 1) + index}">X</div>
-                    </div>` //The long math sequence makes the id match up to the id in the API so the proper Chirp gets the DELETE req. Maybe better way to do it?
-                )
-            }
-        })
+                        <div class="col-1 x" id="${chirp[0]}">X</div>
+                    </div>`
+                    )
+                }
+            })
         $('#chirp-area').append(chirpHtml);
+        let id;
+        let idArray;
+        let idNum;
+        $('.chirp').click((e) => {
+            id = e.currentTarget.id;
+            console.log(id);
+            idArray = id.split('');
+            idArray.splice(0, 3);
+            idNum = idArray.join('');
+            console.log(idNum);
+            $('#name-edit').val($(`#${id}name`).text());
+            $('#chirp-edit').val($(`#${id}chirp`).text());
+            $('#edit-box').modal('show');
+            $('#edit-submit').click(() => {
+                let nameEdit = $('#name-edit').val();
+                let chirpEdit = $('#chirp-edit').val();
+                $(`#${id}name`).text(nameEdit);
+                $(`#${id}chirp`).text(chirpEdit);
+                $.ajax({
+                    type: 'PUT',
+                    url: `http://localhost:3000/api/chirps/${idNum}`,
+                    data: JSON.stringify({
+                        "name": nameEdit,
+                        "text": chirpEdit
+                    }),
+                    contentType: 'application/json; charset=utf-8',
+                    async: true
+                })
+                $('#edit-submit').off();
+                $('#edit-box').modal('hide');
+                $('#chirp-area').empty();
+                getChirps();
+            });
+        });
+        
         $('.x').click((e) => {
             let id = e.target.id;
             $(`#row${id}`).remove();
@@ -26,11 +62,16 @@ const getChirps = () => {
                 type: 'DELETE',
                 url: `http://localhost:3000/api/chirps/${id}`
             })
+            $('#chirp-area').empty();
+            getChirps();
         })
     })
     .catch(err => console.log(err));
 }
+
 getChirps();
+
+
 
 $('#new-chirp-box').hide();
 $('#new-chirp').click(() => {
